8f28b690d976246dcf274f9a85664d1f9b2a49eb
Matt Deitke <mdeitke23@gmail.com>
2021-06-12 21:29:56 -0700

    get graphql working

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,23 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+const path = require(`path`);
+
 module.exports = {
   plugins: [
     `gatsby-plugin-typescript`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-antd`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-emotion`
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: path.join(__dirname, `paper-data`),
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-json`,
+    `gatsby-transformer-sharp`,
   ],
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,46 @@
-import React from "react"
-import Latex from "react-latex"
-import { Button } from "antd"
-import { Helmet } from "react-helmet"
+import React from "react";
+import Latex from "react-latex";
+import { Button } from "antd";
+import { Helmet } from "react-helmet";
 import { css } from "@emotion/react";
 import color from "~styles/color";
+import { graphql } from "gatsby";
+
+interface PaperData {
+  abstract: string;
+  citations: number;
+  title: string;
+  twitter: {
+    likes: number;
+    replies: number;
+    retweets: number;
+  };
+  id: number;
+  s2id: string;
+  pdf: string;
+  authors: string[];
+  arXiv: string;
+}
+
+function Paper(props: PaperData) {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <h2>{props.pdf}</h2>
+    </div>
+  );
+}
 
 /**
  * @param {number} a some number.
  * @param {number} b some other number.
  */
-export default function Home() {
+export default function Home({ data }) {
+  let papers = data.allPaperDataJson.edges;
+  papers = papers.slice(0, 5);
+
+  console.log(papers);
+
   return (
     <div>
       <Helmet>
@@ -17,21 +48,51 @@ export default function Home() {
       </Helmet>
       <h1>Hello, world!</h1>
       <Latex>
-        The center of the universe is at $5+4$. One can also do something like $5+4$.
+        The center of the universe is at $5+4$. One can also do something like
+        $5+4$.
       </Latex>
+      {papers.map((paper: { node: PaperData }) => (
+        <Paper key={paper.node.id} {...paper.node} />
+      ))}
       <br />
       <br />
       <Button type="primary">Hello, world</Button>
-      <div css={css`
-        color: ${color.light.blue6};
-        &:hover {
-          background-color: green;
-        }
-      `}>
+      <div
+        css={css`
+          color: ${color.light.blue6};
+          &:hover {
+            background-color: green;
+          }
+        `}
+      >
         Hello, world
       </div>
       <br />
       <br />
     </div>
-  )
+  );
 }
+
+export const query = graphql`
+  {
+    allPaperDataJson {
+      edges {
+        node {
+          abstract
+          citations
+          title
+          twitter {
+            likes
+            replies
+            retweets
+          }
+          id
+          s2id
+          pdf
+          authors
+          arXiv
+        }
+      }
+    }
+  }
+`;
