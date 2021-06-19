f15d6e63a5533bca8e0d6bfa14503a55099c0fba
Matt Deitke <mdeitke23@gmail.com>
2021-06-19 00:47:03 -0700

    add ~icons/ to relative imports

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,8 @@ exports.onCreateWebpackConfig = ({ actions }) => {
     resolve: {
       alias: {
         "~components": path.resolve(__dirname, "src/components"),
-        "~styles": path.resolve(__dirname, "src/styles")
+        "~styles": path.resolve(__dirname, "src/styles"),
+        "~icons": path.resolve(__dirname, "static/icons"),
       }
     }
   });
